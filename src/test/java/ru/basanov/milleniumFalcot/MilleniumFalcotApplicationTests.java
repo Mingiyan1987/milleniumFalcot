f323f182ee9bcd42ee681bc9032f4e4a515b9cdf
mingiyan1987 <basanov.mt87@gmail.com>
2020-03-09 19:28:34 +0300

    Конфигурирование проекта

---
+++ src/test/java/ru/basanov/milleniumFalcot/MilleniumFalcotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.basanov.milleniumFalcot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilleniumFalcotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
